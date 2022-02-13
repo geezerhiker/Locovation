6d3fff7e085b21c224e0b920fc569d10f1c3121b
Alan Heezen <aheezen@emich.edu>
2022-02-12 16:04:56 -0800

    Initial Commit

---
+++ Locovation/LocovationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocovationApp.swift
+//  Locovation
+//
+//  Created by Alan Heezen on 2/12/22.
+//
+
+import SwiftUI
+
+@main
+struct LocovationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
